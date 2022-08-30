9ad8c133fd81bee5be49a42a4c4fe0b9d54a21f4
shinn <anguaepeng1@gmail.com>
2022-08-30 21:28:24 +0800

    webpage

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-var password = 12345;
-var input;
-var i = 1;
-
-while(password != input){
-    if( i <= 3 ){
-        input = prompt("請輸入密碼");   
-        i++;    
-    } 
-    else{
-        alert("您已被BAN")
-    }
+var page = document.getElementById("page");
+function page(){
+    page.style.opacity = 0;
 }
-
-alert("登入成功!");
+page.addEventListener("mouseleave" , function(){
+    page.style.opacity = 0;
+})
+page.addEventListener("mouseenter" , function(){
+    page.style.opacity = 1;
+})
