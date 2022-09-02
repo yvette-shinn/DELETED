d885698e3776cdad39513b3052b75ff23b213370
shinn <anguaepeng1@gmail.com>
2022-09-02 16:54:37 +0800

    page

--- script.js
+++ script.js
@@ -2,11 +2,5 @@ var page = document.getElementById("page");
 var thr = document.getElementById("thr");
 var body = document.getElementById("body");
 
-thr.addEventListener("mouseover", function(){
-    page.style.left = 0;
-})
-body.addEventListener("mouseover", function(){
-    page.style.position = this.remove;
-})
 
 
